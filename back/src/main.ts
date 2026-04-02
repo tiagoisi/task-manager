b169ba697a0660229d197d78139afaad5ed1baf7
Tiagoisi <tiagoisidromadoery123@gmail.com>
2026-04-01 23:31:03 -0300

    fix: front url

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTENDURL || 'http://localhost5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   });
